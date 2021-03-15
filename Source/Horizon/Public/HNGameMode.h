92dec427ee56d4d514af32b9e05b0f502f11b39b
Antony Melendez <50185974+xpredatorx338@users.noreply.github.com>
2021-03-15 10:03:57 -0700

    created c++ classes

---
+++ Source/Horizon/Public/HNGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HNGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORIZON_API AHNGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
